efeb7a72514d79b1c6b7de9da88351abe1653da3
Beaudry Kock <beaudry.kock@mac.com>
2017-03-14 21:23:51 -0700

    Added in ActionCard

---
+++ DailyAction/ActionDate.h
@@ -0,0 +1,17 @@
+//
+//  ActionDate.h
+//  DailyAction
+//
+//  Created by Beaudry Kock on 3/14/17.
+//  Copyright © 2017 Beaudry Kock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionDate : UIView
+
+@property (nonatomic, strong) IBOutlet UILabel *lb_title;
+@property (nonatomic, strong) IBOutlet UILabel *lb_date;
+@property (nonatomic, strong) IBOutlet UIView *date_container;
+
+@end
